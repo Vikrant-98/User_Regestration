43898b57a2dbe763597cc56303bb27d952b45e6c
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-26 18:59:47 +0530

    Mail Validation added

--- User_Regestration.sh
+++ User_Regestration.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 Name_Regex="^([A-Z]{1}[a-z]{2}[a-z]*)$"
+Mail_Regex="^([a-zA-Z0-9]*[.]*[a-zA-Z0-9]*@[a-zA-Z0-9]*.{1}[a-zA-Z0-9]*[.]*[a-zA-Z0-9]*)$"
 read -p "Enter First Name" First_Name
 	if [[ $First_Name =~ $Name_Regex  ]]
 	then
@@ -14,4 +15,10 @@ read -p "Enter Last Name" Last_Name
 	else
 		echo "invalid"
 	fi
-
+read -p "Enter Mail ID" Mail
+	if [[ $Mail =~ $Mail_Regex  ]]
+	then
+		echo "valid"
+	else
+		echo "invalid"
+	fi
