9b28073ef56189de924623b16a3b51cd55eb4111
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-27 10:04:09 +0530

    Refactor : Mail check Condition

--- User_Regestration.sh
+++ User_Regestration.sh
@@ -130,11 +130,12 @@ if [ $user_Register -eq 5 ]
 then
 	echo "$First_Name $Last_Name Registered Succesfully!!!!!!!!!!!!!!"
 fi
+echo "Checking mail sample valid or not "
 while [ $input -eq 0 ]
 
-	echo "Do you want to check mail sample valid or not "
-	read -p " Enter 0 for yes or any for exit :" input
-
 	Mail$()
+
+	echo "Do you Continue to check mail sample valid or not "
+	read -p " Enter 0 for yes or any for exit :" input
 
 done
